86d4e440db157df3a99cef001aeb07d522eaab01
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2022-11-17 13:06:20 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,14 +1,20 @@
+
+
+
 /* 
 * This example is copied from
 * https://www.thegeekstuff.com/2013/06/buffer-overflow/
 */
+
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char **argv)
 {
-    char buff[15],otherbuf[10];
+    char buff[15];
     int pass = 0;
+
+    printf("\nI understand I will never write a program like this\n");
 
     printf("\n Enter the password : \n");
     scanf("%s",buff);
@@ -26,4 +32,18 @@ int main(int argc, char **argv)
     }
 
     return(0);
-}
+}```
+Then scroll down and "Create a new branch for this commit and start a pull request. "
+
+Then click "Propose changes"
+
+Write a comment and click on "Create pull request"
+
+
+At the "top" you see pull requests are increased, click on pull-requests ...
+
+You will see a pull request we made has a red cross beside it. This means it did not pass the checks.
+
+Select the pull-request
+
+You will see something like
